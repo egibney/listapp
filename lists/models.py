00edf6016dd45e560d69d2923fced8d7c1623995
Emmet Gibney <epgibney@gmail.com>
2012-09-05 00:28:33 -0600

    templates and models revised
    made some changes to the templates styling, and updated the "Item"
    model to include creation date

--- lists/models.py
+++ lists/models.py
@@ -13,6 +13,7 @@ class Item(models.Model):
 	name = models.CharField(max_length=200)
 	list = models.ForeignKey(List)
 	url = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
 
 	def __unicode__(self):
 		return "Item: " + self.name
